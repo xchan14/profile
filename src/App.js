42dda59ae771e4780bc0726d32cb5c699473f23b
Christian Camilon <christiancamilon@gmail.com>
2019-10-02 23:55:19 +0800

    Design update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Box, Avatar, makeStyles, Container, Grid } from '@material-ui/core';
+import { Box, Avatar, makeStyles, Container, Grid, Tab, Tabs, TabPanel } from '@material-ui/core';
 import { ProfileSection } from './components/profile-section/ProfileSection';
 
-const useStyles = makeStyles({
+const profileBoxWidth = 350;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
   avatar: {
     margin: 10,
-    width: 300,
-    height: 300
+    width: 250,
+    height: 250,
+    marginTop: 30,
+    fontSize: 64,
   },
-});
+  profileBox: {
+    height: '100vh',
+    width: profileBoxWidth,
+    backgroundColor: '#D4D4D4'
+  },
+  contentBox: {
+    /*width: `calc(100%-${profileBoxWidth}px)`,*/
+    flexGrow: 1,
+  },
+  tabs: {
+    marginTop: 100,
+    width: 'auto',
+  },
+  tab: {
+    fontSize: 28
+  }
+}));
 
 function App() {
   const classes = useStyles();
   return (
-    <div className="App">
-      <div style={{"marginTop":"300px"}}></div>
-      <Grid container>
-          <Avatar alt="Remy Sharp" href="/logo192.jpg" className={classes.avatar} />
-      </Grid>
-      <ProfileSection title="Cover Page" ></ProfileSection>
-      <ProfileSection title="Career"></ProfileSection>
-      <ProfileSection title="Education"></ProfileSection>
+    <div className="App" className={classes.root}>
+
+      <div className={classes.profileBox}>
+
+        <Grid container justify="center">
+          <Avatar alt="Remy Sharp" className={classes.avatar} >A</Avatar>
+        </Grid>
+
+      </div>
+
+      <div className={classes.contentBox}>
+        <Container >
+          <Tabs className={classes.tabs}
+            centered>
+            <Tab className={classes.tab} label="Career" />
+            <Tab className={classes.tab} label="Education" />
+            <Tab className={classes.tab} label="Contact" />
+          </Tabs>
+          <div hidden={false} >
+            Career dadfaa
+          </div>
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/tab-panels/CareerTab.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function CareerTab(){
+    return(
+        <TabPanel>
+            career
+        </TabPanel>
+    );
+}
